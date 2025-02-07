16bc1d3106b9d60d88b840f5df238d041137eb72
Dru-429 <ect.dhruv@gmail.com>
2025-02-07 12:06:11 +0530

    Db connect

--- app/[handel]/page.jsx
+++ app/[handel]/page.jsx
@@ -3,43 +3,52 @@ import Link from "next/link";
 import clientPromise from "@/lib/mongodb";
 
 const page = async ({ params }) => {
+  const { handel } = params;
 
-  const { handel } = params
+  const client = await clientPromise;
+  const db = client.db("linknext");
+  const collection = db.collection("links");
+  const item = await collection.findOne({ handle: handle });
+  if (!item) {
+    return notFound();
+  }
 
-  const client = await clientPromise
-  
-
-  const item = {
-    _id: {
-      $oid: "6729e97390cf30c8f66c4c68",
-    },
-    links: [
-      {
-        link: "https://www.instagram.com/codewithharry/?hl=en",
-        linktext: "Instagram",
-      },
-      {
-        link: "https://www.codewithharry.com",
-        linktext: "Website",
-      },
-      {
-        link: "https://www.YouTube.com/codewithharry/?hl=en",
-        linktext: "YouTube",
-      },
-    ],
-    handle: "Dru",
-    pic: "https://avatars.githubusercontent.com/u/146123722?s=400&u=8d1f74e617db5be4e02507c09758b624fed4bdac&v=4",
-    desc: "Hisashiburi...Watashi wa Dru 💫 I'm Dru, passionate frontend developer from India"
-  };
+  // const item2 = {
+  //   _id: {
+  //     $oid: "6729e97390cf30c8f66c4c68",
+  //   },
+  //   links: [
+  //     {
+  //       link: "https://www.instagram.com/codewithharry/?hl=en",
+  //       linktext: "Instagram",
+  //     },
+  //     {
+  //       link: "https://www.codewithharry.com",
+  //       linktext: "Website",
+  //     },
+  //     {
+  //       link: "https://www.YouTube.com/codewithharry/?hl=en",
+  //       linktext: "YouTube",
+  //     },
+  //   ],
+  //   handle: "Dru",
+  //   pic: "https://avatars.githubusercontent.com/u/146123722?s=400&u=8d1f74e617db5be4e02507c09758b624fed4bdac&v=4",
+  //   desc: "Hisashiburi...Watashi wa Dru 💫 I'm Dru, passionate frontend developer from India",
+  // };
 
   return (
     <div className="flex min-h-screen bg-purple-400 justify-center items-start py-10">
-
       {item && (
         <div className="photo flex justify-center flex-col items-center gap-4">
-          <img src={item.pic} alt="profile-pic" className="h-[10vh] w-[10vh] rounded-full" />
+          <img
+            src={item.pic}
+            alt="profile-pic"
+            className="h-[10vh] w-[10vh] rounded-full"
+          />
           <span className="font-bold text-xl">{handel}</span>
-          <span className="desc w-80 text-center text-zinc-900">{item.desc}</span>
+          <span className="desc w-80 text-center text-zinc-900">
+            {item.desc}
+          </span>
           <div className="links">
             {item.links.map((item, index) => {
               return (
@@ -53,7 +62,6 @@ const page = async ({ params }) => {
           </div>
         </div>
       )}
-
     </div>
   );
 };
